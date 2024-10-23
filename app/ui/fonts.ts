cbf9eda2befa72423e894e3d8779f3f837e387e0
sauravxc1 <142659565+sauravxc1@users.noreply.github.com>
2024-10-23 11:58:34 +0530

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Set up the Inter font
+export const inter = Inter({ subsets: ['latin'] });
+
+// Set up the Lusitana font with specified weights
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
